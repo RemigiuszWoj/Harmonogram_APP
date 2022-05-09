78d28f4abf4da72ef0af90e68fff8a232d27a0f2
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-05-09 21:21:19 +0200

    09_05_2022

--- scr/main.py
+++ scr/main.py
@@ -2,6 +2,8 @@
 import parser
 import graph
 import algorithm
+import copy
+import random
 
 def load_orders() -> list:
     orders = parser.PARS_ORDERS()
@@ -41,6 +43,8 @@ def get_job():
     G_list.append(G)
     G1 = parser.PARSOWANIE_DANYCH(path ="dryer_20_t.csv", nr_job = 2)
     G_list.append(G1)
+    G2 = parser.PARSOWANIE_DANYCH(path ="dryer_20_t.csv", nr_job = 3)
+    G_list.append(G2)
     return G_list
 
 def create_G(G_list, sequence, nb):
@@ -139,7 +143,7 @@ def IS_TOP(pi, Graph):
                 return False
         return True
 
-def optimze_c_max(Graph, workers_list, ord):
+def insert_nbr(Graph, workers_list, ord):
     best_C, best_max_C = 0, 0
     a0 = easy_asign(Graph=Graph, workers=workers_list)
 
@@ -148,12 +152,14 @@ def optimze_c_max(Graph, workers_list, ord):
 
     best_C = C0
     best_max_C = max_C0
+    best_ord = copy.deepcopy(ord)
 
     for i in range(1,len(ord)):
-        for j in range(1,len(ord)):
+        for j in range(i+1,len(ord)):
             move_elem(l=ord,oldindex=i,newindex=j)
             if IS_TOP(pi=ord,Graph=Graph) == False:
                 move_elem(l=ord,oldindex=j,newindex=i)
+                break
             else:
                 a1 = 0
                 C1, max_C1 = 0, 0
@@ -166,25 +172,147 @@ def optimze_c_max(Graph, workers_list, ord):
                 if best_max_C > max_C1:
                     best_max_C = max_C1
                     best_C = C1
-    return best_C, best_max_C
+                    best_ord = copy.deepcopy(ord)
 
+                move_elem(l=ord,oldindex=j,newindex=i)
+        for j in range(i-1,1,-1):
+            move_elem(l=ord,oldindex=i,newindex=j)
+            if IS_TOP(pi=ord,Graph=Graph) == False:
+                move_elem(l=ord,oldindex=j,newindex=i)
+                break
+            else:
+                a1 = 0
+                C1, max_C1 = 0, 0
+
+                a1 = easy_asign(Graph=G, workers=workers_list)
+
+                C1, max_C1 = c_max2(m=len(workers_list),
+                workers=workers_list,Graph=Graph, a=a1, pi=ord)
+
+                if best_max_C > max_C1:
+                    best_max_C = max_C1
+                    best_C = C1
+                    best_ord = copy.deepcopy(ord)
+
+                move_elem(l=ord,oldindex=j,newindex=i)
+    return best_C, best_max_C, best_ord
+
+
+
+
+def insert_rand(Graph, workers_list, ord):
+    best_C, best_max_C = 0, 0
+    a0 = easy_asign(Graph=Graph, workers=workers_list)
+
+    C0, max_C0 = c_max2(m=len(workers_list),
+    workers=workers_list,Graph=Graph, a=a0, pi=ord)
+
+    best_C = C0
+    best_max_C = 1000_000
+    best_ord = copy.deepcopy(ord)
+    i = random.randint(1,len(ord)-1)
+    # print(i)
+    # print(len(ord))
+    for j in range(i+1,len(ord)):
+        move_elem(l=ord,oldindex=i,newindex=j)
+        if IS_TOP(pi=ord,Graph=Graph) == False:
+            move_elem(l=ord,oldindex=j,newindex=i)
+            break
+        else:
+            a1 = 0
+            C1, max_C1 = 0, 0
+
+            a1 = easy_asign(Graph=G, workers=workers_list)
+
+            C1, max_C1 = c_max2(m=len(workers_list),
+            workers=workers_list,Graph=Graph, a=a1, pi=ord)
+
+            if best_max_C > max_C1:
+                best_max_C = max_C1
+                best_C = C1
+                best_ord = copy.deepcopy(ord)
+
+            move_elem(l=ord,oldindex=j,newindex=i)
+    for j in range(i-1,1,-1):
+        move_elem(l=ord,oldindex=i,newindex=j)
+        if IS_TOP(pi=ord,Graph=Graph) == False:
+            move_elem(l=ord,oldindex=j,newindex=i)
+            break
+        else:
+            a1 = 0
+            C1, max_C1 = 0, 0
+
+            a1 = easy_asign(Graph=G, workers=workers_list)
+
+            C1, max_C1 = c_max2(m=len(workers_list),
+            workers=workers_list,Graph=Graph, a=a1, pi=ord)
+
+            if best_max_C > max_C1:
+                best_max_C = max_C1
+                best_C = C1
+                best_ord = copy.deepcopy(ord)
+
+            move_elem(l=ord,oldindex=j,newindex=i)
+    return best_C, best_max_C, best_ord
 
 workers_data = parser.PARS_WORKERS()
 wokrers_list = parser.preper_woreks(workers_data=workers_data)
 
-sequence =[0, 1, 2]
+sequence =[0, 1, 2, 3]
 nb = 2
 
 G_list = get_job()
 
 G = create_G(G_list=G_list, sequence=sequence, nb=nb) 
 ord = G.TOP_ORDER()
+# print(ord)
 
-best_C, best_max_C = optimze_c_max(Graph=G, workers_list=wokrers_list, ord=ord)
 
-print(ord)
-print("best_C: ", best_C)
-print("best_max_C: ", best_max_C)
+def ds(ord, Graph, workers_list):
+    best_ord = copy.deepcopy(ord)
+    NX = 1000_000
+    while True:
+
+        best_C, best_max_C, best_ord = insert_nbr(Graph=Graph, workers_list=workers_list, ord=best_ord)
+        if best_max_C < NX:
+            NX = best_max_C
+        else:
+            break
+        print("best_max_C: ", best_max_C)
+
+    return best_C, best_max_C, best_ord
+
+# best_C, best_max_C, best_ord=ds(ord=ord, Graph=G, workers_list=wokrers_list)
+best_ord = copy.deepcopy(ord)
+NX = 1000_000
+for i in range(1000):
+    best_C, best_max_C, best_ord = insert_rand(Graph=G, workers_list=wokrers_list, ord=best_ord)
+    if best_max_C < NX:
+        NX = best_max_C
+        print("best_max_C: ", best_max_C)
+
+
+#to co mam zapisać jako rand
+#symulowane wyzazanie na podstawie tego co wyzej
+
+
+
+
+
+
+# best_C, best_max_C, best_ord = optimze_c_max(Graph=G, workers_list=wokrers_list, ord=ord)
+
+# print(best_ord)
+# best_C, best_max_C, best_ord = optimze_c_max(Graph=G, workers_list=wokrers_list, ord=best_ord)
+
+# print(best_ord)
+# est_C, best_max_C, best_ord = optimze_c_max(Graph=G, workers_list=wokrers_list, ord=best_ord)
+
+# print(best_ord)
+
+
+# # print("best_C: ", best_C)
+# print("best_max_C: ", best_max_C)
 
 
 # print(len(ord))
